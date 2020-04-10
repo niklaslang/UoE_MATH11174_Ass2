d48c193dc43c9a3fc09ed62f53c5591b2fdedf05
niklaslang <niklasjonathanlang@gmail.com>
2020-04-10 11:42:58 +0200

    problem4 - b - 100%

--- problem4.R
+++ problem4.R
@@ -5,6 +5,7 @@ library(data.table)
 # Reading file `nki.csv` into a data table named `nki.dt`:
 
 nki.dt <- data.table(read.csv("nki.csv", stringsAsFactors = TRUE))
+nki.genes.dt <- nki.dt[, -c(1,2,3,4,5,6)]
 
 # Computing the matrix of correlations between the gene expression variables, 
 # and displaying it so that a block structure is highlighted:
@@ -13,7 +14,7 @@ nki.dt <- data.table(read.csv("nki.csv", stringsAsFactors = TRUE))
 library(corrplot)
 
 # computing the correlation between the numerical colums `nki.dt`
-nki.corr <- cor(nki.dt[, .SD, .SDcols = sapply(nki.dt, is.numeric)], use="pairwise.complete")
+nki.corr <- cor(nki.genes.dt, use="pairwise.complete")
 
 # correlation plot
 par(cex=0.5)
@@ -35,7 +36,7 @@ for (i in 1:nrow(nki.corr)){
   for (j in 1:ncol(nki.corr)){
     if(i < j){
       if(abs(nki.corr[i,j]) > 0.8){
-        distinct.unique.pair <- c(rownames(nki.corr)[i], colnames(nki.corr)[j],nki.corr[i,j])
+        distinct.unique.pair <- c(rownames(nki.corr)[i], colnames(nki.corr)[j], signif(nki.corr[i,j],3))
         corr.pairs <- rbind(corr.pairs, distinct.unique.pair)
       }
     }
@@ -49,3 +50,25 @@ corr.pairs.dt
 
 ### (b) ###
 
+# Running PCA (only over the columns containing gene expressions) 
+# so that it is possible to identify variable clusters
+# Producing a scatter plot of the projection of the predictors on the first two principal components 
+# and reporting the percentage of variance explained by the first two components. 
+
+# assertion check whether there are any missing values in nki.dt
+stopifnot(sum(is.na(nki.genes.dt)) == 0)
+
+pca.genes <- prcomp(t(nki.genes.dt), scale = TRUE)
+
+# scatter plot: projection of the predictors on the first two principal components
+par(cex=1)
+plot(pca.genes$x[, 1:2], main="Projection of variables on the first 2 PCs", col = "firebrick", cex=0.8)
+
+# percentage of variance explained by the first two components
+perc.expl <- pca.genes$sdev^2 / sum(pca.genes$sdev^2) 
+sum(perc.expl[1:2])
+
+# Starting from the PCA plot just produced, devising a simple rule PC2 < -10 
+# that identifies the four gene expressions that are most different from the rest and report their names
+pca.genes$x[which(pca.genes$x[,"PC2"] < -10), .SD]
+  
